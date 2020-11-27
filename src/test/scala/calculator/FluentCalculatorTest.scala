4a7148a7869a1dfb9dafa33d65f0aad9b6a89c5c
saljuama <saljuama@gmail.com>
2020-11-27 03:41:20 +0100

    make the test a bit more readable

--- src/test/scala/calculator/FluentCalculatorTest.scala
+++ src/test/scala/calculator/FluentCalculatorTest.scala
@@ -1,27 +1,36 @@
 package calculator
 
+import org.scalatest.BeforeAndAfterEach
 import org.scalatest.matchers.must.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
-class FluentCalculatorTest extends AnyWordSpec with Matchers {
+class FluentCalculatorTest extends AnyWordSpec with Matchers with BeforeAndAfterEach {
 
-  "The fluent calculator" can {
+  private var calculator: FluentCalculator = _
+  override protected def beforeEach(): Unit = {
+    calculator = new FluentCalculator()
+  }
+
+  "The fluent calculator" when {
     import FluentCalculator.calcToInt
 
-    "calc" when {
-      Seq(0, 200, -150).foreach(value => s"for value $value" in {
-        val result: Int = new FluentCalculator().calc(value)
+    "calc" can  {
+      Seq(0, 200, -150).foreach(value => s"initialize the calculator with value $value" in {
+        val result: Int = calculator
+          .calc(value)
         result mustBe value
       })
     }
 
-    "plus" when {
+    "plus" can  {
       "add a value to the initial value" in {
-        val result: Int = new FluentCalculator().calc(10).plus(5)
+        val result: Int = calculator
+          .calc(10)
+          .plus(5)
         result mustBe 15
       }
       "chain multiple calls" in {
-        val result: Int = new FluentCalculator()
+        val result: Int = calculator
           .calc(10)
           .plus(5)
           .plus(10)
@@ -30,45 +39,67 @@ class FluentCalculatorTest extends AnyWordSpec with Matchers {
       }
     }
 
-    "minus" when {
+    "minus" can  {
       "subtract values to the initial value" in {
-        val result: Int = new FluentCalculator().calc(10).minus(5)
+        val result: Int = calculator
+          .calc(10)
+          .minus(5)
         result mustBe 5
       }
       "chain multiple calls" in {
-        val result: Int = new FluentCalculator().calc(10).minus(5).plus(10).minus(15)
+        val result: Int = calculator
+          .calc(10)
+          .minus(5)
+          .plus(10)
+          .minus(15)
         result mustBe 0
       }
     }
 
-    "undo" when {
-      "removes last operation" in {
-        val result: Int = new FluentCalculator().calc(10).plus(20).undo()
+    "undo" can  {
+      "discard the last operation" in {
+        val result: Int = calculator
+          .calc(10)
+          .plus(20)
+          .undo()
         result mustBe 10
       }
-      "does nothing when there are no operations" in {
-        val result: Int = new FluentCalculator().calc(10).undo()
+      "does nothing when there are no pending operations" in {
+        val result: Int = calculator
+          .calc(10)
+          .undo()
         result mustBe 10
       }
     }
 
-    "redo" when {
-      "adds the last undone operation" in {
-        val result: Int = new FluentCalculator().calc(10).plus(10).undo().redo()
+    "redo" can  {
+      "restore the last undone operation" in {
+        val result: Int = calculator
+          .calc(10)
+          .plus(10)
+          .undo()
+          .redo()
         result mustBe 20
       }
-      "does nothing when there are no operations" in {
-        val result: Int = new FluentCalculator().calc(10).redo()
+      "does nothing when there are no discarded operations" in {
+        val result: Int = calculator
+          .calc(10)
+          .redo()
         result mustBe 10
       }
     }
 
-    "save" when {
+    "save" can  {
       "prevents undo and redo to have effect on operations before save" in {
-        val result: Int = new FluentCalculator().calc(10).plus(10).save().undo().redo().plus(5)
+        val result: Int = calculator
+          .calc(10)
+          .plus(10)
+          .save()
+          .undo()
+          .redo()
+          .plus(5)
         result mustBe 25
       }
     }
   }
-
 }
